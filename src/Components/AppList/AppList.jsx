3f600583de839edf22966e7f7a3bf881047991de
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-01-11 08:16:49 +0530

    3rd commit

---
+++ src/Components/AppList/AppList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppList = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default AppList
---
+++ src/Components/AppMap/AppMap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppMap = () => {
+  return (
+    <div>Map</div>
+  )
+}
+
+export default AppMap
